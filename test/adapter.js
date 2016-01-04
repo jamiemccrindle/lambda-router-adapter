d124ec8aa5c3113699f6a18bb185deb4fc999ee6
Jamie McCrindle <jamiemccrindle@gmail.com>
2016-01-04 22:18:25 +0000

    more tests

--- test/adapter.js
+++ test/adapter.js
@@ -19,6 +19,24 @@ describe('LambdaRouterAdapter', function() {
         done();
       }});
     });
+    it('should 404', function (done) {
+      var index = require('../');
+      var app = express();
+
+      app.get('/status', function(req, res){
+        res.send('OK');
+      });
+
+      app.use(function(req, res){
+        res.status(404).send('NotFound');
+      });
+
+      index.newExpressHandler(app)({url: '/test', method: 'GET'}, {done: function(error, lambdaResponse) {
+        assert.ifError(error);
+        assert.equal(404, lambdaResponse.statusCode);
+        done();
+      }});
+    });
   });
   describe('request', function () {
     it('should compile', function () {
