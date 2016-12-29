dd7f524ee83ab7f03cd06fd5276599aa5e8f80a9
Hendrik <hsch@users.noreply.github.com>
2016-12-29 09:50:14 +0100

    Detect and use latest Lambda programming model, which fixes #7

--- index.js
+++ index.js
@@ -12,17 +12,27 @@ var response = require('./response');
  * @param {httpCallback} callback
  * @returns {Function}
  */
-function newExpressHandler(app) {
+function newExpressHandler(app, callbackWaitsForEmptyEventLoop) {
   app.request.__proto__.__proto__ = request.LambdaHttpRequest.prototype;
   app.response.__proto__.__proto__ = response.LambdaHttpResponse.prototype;
-  return function(event, context) {
+  return function(event, context, callback) {
+    if (typeof callback != 'undefined' && typeof callbackWaitsForEmptyEventLoop == 'undefined') {
+      // The callback is defined -> we're running in the new Node.js runtime,
+      // and callbackWaitsForEmptyEventLoop is not explicitly defined -> we should apply the transition pattern:
+      // http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-using-old-runtime.html#transition-to-new-nodejs-runtime 
+      context.callbackWaitsForEmptyEventLoop = false;
+    }
+    else if (typeof callbackWaitsForEmptyEventLoop != 'undefined') {
+      // The user code passed callbackWaitsForEmptyEventLoop -> apply this value.
+      context.callbackWaitsForEmptyEventLoop = callbackWaitsForEmptyEventLoop;
+    }
     var lambdaHttpRequest = request.convert(event);
     var lambdaHttpResponse = response.newResponse(function(lambdaHttpResponse) {
-      context.done(null, response.convert(lambdaHttpResponse));
+      (callback || context.done)(null, response.convert(lambdaHttpResponse));
     });
     app.handle(lambdaHttpRequest, lambdaHttpResponse, function(error) {
       if(error) {
-        context.done(error);
+        (callback || context.done)(error);
       }
     });
   }
